053c9bd3e306ae4833caef41a528a444c658ed85
Roman Ammann <m3ccanico@xxo.ch>
2018-07-15 10:09:41 +1200

    added Doc Strings and comments, standardised on double speech marks

--- lxd2influx.py
+++ lxd2influx.py
@@ -15,30 +15,37 @@ import requests_unixsocket
 requests_unixsocket.monkeypatch()
 
 INFLUX_DB_PORT = 8086
-INFLUX_DB_IP = '172.31.40.72'
-INFLUX_DB_NAME = 'lxd'
+INFLUX_DB_IP = "172.31.40.72"
+INFLUX_DB_NAME = "lxd"
 
-CGROUP_PREFIX = '/sys/fs/cgroup'
+CGROUP_PREFIX = "/sys/fs/cgroup"
 
 STATUS_RUNNING = 103
 STATUS_SUCCESS = 200
 
 INTERVAL = 10
 
-localtz = timezone('Pacific/Auckland')
+localtz = timezone("Pacific/Auckland")
 
 
 def main():
+    """ Main loop, opens the connection to InfluxDB and starts the server loop """
 
     hostname = platform.node()
 
     # connect to DB
     influx = InfluxDBClient(INFLUX_DB_IP, INFLUX_DB_PORT,
-                            '', '', INFLUX_DB_NAME)
+                            "", "", INFLUX_DB_NAME)
     server(hostname, influx)
 
 
 def server(hostname, influx):
+    """ Triggers the update_meassurement function every INTERVAL
+
+    Args:
+        hostname: The name of the system as a string
+        influx: The InfluxDB client object    
+    """ 
 
     done = 0
 
@@ -51,11 +58,22 @@ def server(hostname, influx):
 
 
 def update_meassurement(hostname, influx, ts):
-    ts = datetime.fromtimestamp(ts, localtz)
-    ts_formated = ts.strftime('%Y-%m-%dT%H:%M:%S%Z')
+    """ Reads the different LXD meassurements (CPU, memory, disk, network 
+    traffic) and sends it to InfluxDB.
 
-    r = requests.get('http+unix://%2Fvar%2Flib%2Flxd%2F'
-                     'unix.socket/1.0/containers')
+    Args:
+        hostname: The name of the system as a string
+        influx: The InfluxDB client object
+        ts: The current timestamp as integer (seconds since epoch) 
+    """ 
+
+    # Create an InfluxDB friendly timestamp as string
+    ts = datetime.fromtimestamp(ts, localtz)
+    ts_formated = ts.strftime("%Y-%m-%dT%H:%M:%S%Z")
+
+    # Read the list of containers from the local Unix socket
+    r = requests.get("http+unix://%2Fvar%2Flib%2Flxd%2F"
+                     "unix.socket/1.0/containers")
     if r.status_code == STATUS_SUCCESS:
         json = r.json()
     else:
@@ -63,23 +81,25 @@ def update_meassurement(hostname, influx, ts):
 
     measurements = []
 
-    for container in json['metadata']:
-        container_name = container.split('/')[-1]
+    # iterate over the local containers
+    for container in json["metadata"]:
+        container_name = container.split("/")[-1]
 
-        r = requests.get('http+unix://%2Fvar%2Flib%2Flxd%2F'
-                         'unix.socket/1.0/containers/'
-                         '{}/state'.format(container_name))
+        # get the state information of the container
+        r = requests.get("http+unix://%2Fvar%2Flib%2Flxd%2F"
+                         "unix.socket/1.0/containers/"
+                         "{}/state".format(container_name))
         if r.status_code == STATUS_SUCCESS:
             json = r.json()
 
-            if int(json['metadata']['status_code']) == STATUS_RUNNING:
+            if int(json["metadata"]["status_code"]) == STATUS_RUNNING:
 
                 # CPU
-                # /sys/fs/cgroup/cpu/lxc/*/cpuacct.stat
+                # read /sys/fs/cgroup/cpu/lxc/<container_name>/cpuacct.stat
                 with open(
-                    os.path.join(CGROUP_PREFIX, 'cpu',
-                                 'lxc', container_name,
-                                 'cpuacct.stat'), 'r') as cgroup:
+                    os.path.join(CGROUP_PREFIX, "cpu",
+                                 "lxc", container_name,
+                                 "cpuacct.stat"), "r") as cgroup:
                     lines = cgroup.read().splitlines()
 
                     cpu_user = 0
@@ -109,7 +129,7 @@ def update_meassurement(hostname, influx, ts):
                     measurements.append(measurement)
 
                 # Memory
-                usage_in_bytes = int(json['metadata']['memory']['usage'])
+                usage_in_bytes = int(json["metadata"]["memory"]["usage"])
                 measurement = {
                     "measurement": "memory",
                     "tags": {
@@ -124,10 +144,10 @@ def update_meassurement(hostname, influx, ts):
                 measurements.append(measurement)
 
                 # network interfaces
-                for interface in json['metadata']['network']:
-                    cntrs = json['metadata']['network'][interface]['counters']
+                for interface in json["metadata"]["network"]:
+                    cntrs = json["metadata"]["network"][interface]["counters"]
                     # only include eth interfaces
-                    if re.match('^eth', interface):
+                    if re.match("^eth", interface):
                         measurement = {
                             "measurement": "network",
                             "tags": {
@@ -137,16 +157,16 @@ def update_meassurement(hostname, influx, ts):
                             },
                             "time": ts_formated,
                             "fields": {
-                                "rx_bytes":   int(cntrs['bytes_received']),
-                                "tx_bytes":   int(cntrs['bytes_sent']),
-                                "rx_packets": int(cntrs['packets_received']),
-                                "tx_packets": int(cntrs['packets_sent']),
+                                "rx_bytes":   int(cntrs["bytes_received"]),
+                                "tx_bytes":   int(cntrs["bytes_sent"]),
+                                "rx_packets": int(cntrs["packets_received"]),
+                                "tx_packets": int(cntrs["packets_sent"]),
                             }
                         }
                         measurements.append(measurement)
 
                 # disk
-                for disk in json['metadata']['disk']:
+                for disk in json["metadata"]["disk"]:
                     measurement = {
                         "measurement": "disk",
                         "tags": {
@@ -156,13 +176,14 @@ def update_meassurement(hostname, influx, ts):
                         },
                         "time": ts_formated,
                         "fields": {
-                            "usage":   json['metadata']['disk'][disk]['usage']
+                            "usage":   json["metadata"]["disk"][disk]["usage"]
                         }
                     }
                     measurements.append(measurement)
 
+    # send all collected meassurements to InfluxDB
     influx.write_points(measurements)
 
-
+# start the main function if the file is called directly
 if __name__ == "__main__":
     main()
